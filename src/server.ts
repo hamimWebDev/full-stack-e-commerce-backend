5e2b095bf38fcad0c92ea1c5878e31a5fd42c633
hamimDev <mdhamimhowladerasif@gmail.com>
2025-04-18 01:29:35 +0600

    solved cors issue

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'ALL'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
